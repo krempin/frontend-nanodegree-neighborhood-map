f7850e662ecb04663544cffdd8c0d03e221509ff
FK <friederike@krempin.it>
2018-09-01 11:17:08 +0200

    feat: Get temple coordinates

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
+import TempleState from './state'
 import Sidebar from './components/Sidebar/Sidebar'
 import TempleMap from './components/TempleMap'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Sidebar />
-        <TempleMap />
-      </div>
-    );
-  }
+  	constructor() {
+    	super()
+    	this.state = TempleState
+  	}
+
+  	render() {
+    	return (
+      		<div className="App">
+        		<Sidebar />
+        		<TempleMap />
+      		</div>
+    	);
+  	}
 }
 
 export default App;
---
+++ src/state.js
@@ -0,0 +1,58 @@
+export default {
+  'temples': [{
+      id: 0,
+      active: false,
+      visible: true,
+      title: 'Fushimi Inari Taisha',
+      unsplashQuery: 'Fushimi inari taisha',
+      position: {
+        lat: 34.9671402,
+        lng: 135.7726717
+      },
+    },
+    {
+      id: 1,
+      active: false,
+      visible: true,
+      title: 'Kinkaku-Ji',
+      unsplashQuery: 'Kinkakuji',
+      position: {
+        lat: 35.0393744,
+        lng: 135.7270544
+      },
+    },
+    {
+      id: 2,
+      active: false,
+      visible: true,
+      title: 'Ginkaku-ji',
+      unsplashQuery: 'Kyoto temple',
+      position: {
+        lat: 35.0270257,
+        lng: 135.7960171
+      },
+    },
+    {
+      id: 3,
+      active: false,
+      visible: true,
+      title: 'Nanzen-ji',
+      unsplashQuery: 'Sanzenin',
+      position: {
+        lat: 35.0150253,
+        lng: 135.7580899
+      },
+    },
+    {
+      id: 4,
+      active: false,
+      visible: true,
+      title: 'Kiyomizu Dera',
+      unsplashQuery: 'kiyomizu-dera',
+      position: {
+        lat: 34.9948353,
+        lng: 135.78495299999997
+      },
+    }
+  ],
+}
