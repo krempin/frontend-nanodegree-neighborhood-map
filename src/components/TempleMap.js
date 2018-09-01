022b4d738d0da89bce7b90855686b217aed617c6
FK <friederike@krempin.it>
2018-09-01 11:13:51 +0200

    feat: Add google-maps-react

--- src/components/TempleMap.js
+++ src/components/TempleMap.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react'
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
 class TempleMap extends Component {
   render() {
 
     return (
       <div> 
-        Temple Map
+        <Map 
+          temples={this.props.temples}
+          initialCenter={{lat: 35.02107, lng: 135.75385}} 
+          onClick={this.unselectMarker}
+          google={this.props.google} // listen for clicks on the map
+          zoom={12} // zoom level
+        />
       </div>
     );
   }
 }
 
+export {TempleMap}
 
-export default TempleMap
+export default GoogleApiWrapper({
+  apiKey: ('AIzaSyBmQO60Q-VZ2VKlFxSUxYZy-GMaj1aWxVM')
+})(TempleMap)
