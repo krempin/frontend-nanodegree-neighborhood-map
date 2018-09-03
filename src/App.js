78bc79cec907c16445dc27a15738a11d9e1a01bd
FK <friederike@krempin.it>
2018-09-03 19:21:58 +0200

    style: Rearrange App.js

--- src/App.js
+++ src/App.js
@@ -1,62 +1,60 @@
-import React, { Component } from 'react';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.min.css';
-import './App.css';
+import React, { Component } from 'react'
+import { ToastContainer } from 'react-toastify'
 
-import TempleState from './state'
 import Sidebar from './components/Sidebar/Sidebar'
 import TempleMap from './components/TempleMap'
+import TempleState from './state'
+
+import 'react-toastify/dist/ReactToastify.min.css'
+import './App.css'
 
 class App extends Component {
-  	constructor() {
-    	super()
-    	this.state = TempleState
-  	}
+  constructor() {
+    super()
+    this.state = TempleState
+  }
 
-	/* Update temples state by its ID */
+  /* Update temples state by its ID */
+  updateTemple = (temple, values) => {
+    if (temple) {
+      this.setState({
+        temples: this.state.temples.map(t =>
+          t.id === temple.id ? Object.assign(t, values) : t
+        )
+      })
+    }
+  }
 
-	updateTemple = (temple, values) => {
-	    if (temple) {
-	      	this.setState({
-	        	temples: this.state.temples.map(t =>
-	          	t.id === temple.id ? Object.assign(t, values) : t
-	        	)
-	      	})
-	    }
-	}
+  /* Sets the state of the clicked temple as active and deactives the previous one */
+  setActiveTemple = (temple) => {
+    this.updateTemple(
+      this.state.temples.find(t => t.active),
+      { active: false }
+    )
 
-  	/* Sets the state of the clicked temple as active and deactives the previous one */
+    if (temple) {
+      this.updateTemple(temple, {active: true})
+    }
+  }
 
-  	setActiveTemple = (temple) => {
-    	this.updateTemple(
-      		this.state.temples.find(t => t.active), 
-      		{ active: false }
-    	)
+  render() {
+    return (
+      <div className="App">
+        <Sidebar
+          temples={this.state.temples}
+          setActiveTemple={this.setActiveTemple}
+          updateTemple={this.updateTemple}
+        />
+        <TempleMap
+          temples={this.state.temples}
+          setActiveTemple={this.setActiveTemple}
+          updateTemple={this.updateTemple}
+        />
 
-    	if (temple) {
-      		this.updateTemple(temple, {active: true})
-    	}
-  	}
-
-  	render() {
-    	return (
-    		<div className="App">
-      		<Sidebar
-            temples={this.state.temples} 
-            setActiveTemple={this.setActiveTemple}
-            updateTemple={this.updateTemple} 
-          />
-      		<TempleMap 
-      			temples={this.state.temples} 
-      			setActiveTemple={this.setActiveTemple}
-            updateTemple={this.updateTemple} 
-      		/>
-
-          <ToastContainer />
-
-    		</div>
-    	);
-  	}
+        <ToastContainer />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
