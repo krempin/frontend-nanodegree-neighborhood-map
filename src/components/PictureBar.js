aa0ec6faf6889202e5ec6083673c49c886a0eb26
FK <friederike@krempin.it>
2018-09-03 18:56:29 +0200

    refactor: Add variables, className, delete consolelog

--- src/components/PictureBar.js
+++ src/components/PictureBar.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react'
 import axios from 'axios'
 import { toast } from 'react-toastify'
+import React, { Component } from 'react'
 
 class PictureBar extends Component {
   constructor(props) {
@@ -11,36 +11,31 @@ class PictureBar extends Component {
     }
 
     this.baseUrl = 'https://api.unsplash.com/search/photos?page=1'
+    this.authHeader = 'Client-ID 779f7137119bb361f91f6bce7a9f888d34db054abfc1714b5ec10185e5c0f8d8'
   }
 
   /* Initializes an update of the displayed images - but only if there is a changed
     query to prevent never ending request-loops. */
-
   componentDidUpdate(prevProps) {
     this.props.query && this.props.query !== prevProps.query && this.loadImage()
   }
 
   /* Updates the component-state with the results of the current query-property
     or inform the user about connection-problems. */
-
   loadImage() {
     axios
       .get(`${this.baseUrl}&query=${this.props.query}`, {
-        // dev-hint: remove headers for testing the error-case
         headers: {
-          Authorization: 'Client-ID 779f7137119bb361f91f6bce7a9f888d34db054abfc1714b5ec10185e5c0f8d8'
+          Authorization: this.authHeader
         }
       })
       .then(res => {
-        console.log(res.data.results);
-        
         this.setState({
-          // good place to sort them (e.g by likes)
           pictures: res.data.results
         })
       })
       .catch(e => {
-        this.props.unselect()        
+        this.props.unselect()
         toast.error("Couldn't load related images. Sorry :(")
       })
   }
@@ -49,19 +44,19 @@ class PictureBar extends Component {
     return (
       <div id="image-bar" className={this.props.active ? 'act' : ''}>
         {this.state.pictures.map(p =>
-          <a key={p.id} 
+          <a key={p.id}
             href={p.urls.raw}
             target="_blank"
-            className="picture" 
+            className="picture"
             style={{backgroundImage: `url(${p.urls.small})`}}>
 
             <span className="info-overlay">
               <span className="likes" data-likes={p.likes}></span>
               <span className="descr">{p.description}</span>
             </span>
-          </a> 
+          </a>
         )}
-        <div class="credit">
+        <div className="credit">
           Images provided by <a href="https://unsplash.com/" target="_blank" rel="noopener noreferrer">Unsplash.com</a>
         </div>
       </div>
